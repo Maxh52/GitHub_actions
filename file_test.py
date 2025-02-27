cd75bfb89acaef2b9ac82d825de71adbba8b0558
Maxh52 <maxhenon@gmail.com>
2025-02-27 13:29:19 +0000

    Ajout des fichiers file_test.py et control_tests.yaml

---
+++ file_test.py
@@ -0,0 +1,19 @@
+import pytest
+def test_calc_addition():
+	# Fonction test du resultat de 2+4
+	output = 2+4
+	assert output == 6
+
+def test_calc_substraction():
+	# fonctione test du résultat de 2-4
+	output = 2 - 4
+	assert output == -2
+
+def test_calc_multiply():
+	output = 2*4
+	assert ouput == 8
+
+def test_coucpou():
+	output = "hello"
+	assert = output == "hello"
+
